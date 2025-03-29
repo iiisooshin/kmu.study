83b768f963a19071dacaee95f4583b2c282b625f
iiisooshin <leesu020806@stu.kmu.ac.kr>
2025-03-29 21:32:35 +0900

    0327_11완성

--- 11완성_신이수_5701835.c
+++ 11완성_신이수_5701835.c
@@ -14,32 +14,54 @@ typedef struct StdType{
 
 int main(){
 
-    int n;
-    // StdType* sptr = students;
+    int size, i;
 
     printf("몇명의 학생 정보를 입력하시나요? : ");
-    scanf("%d",&n);
+    scanf("%d",&size);
+    getchar(); //버퍼 비우기
 
-    for(int i = 0; i < n; i++){
-        printf("[%d] 학생의 정보를 입력하세요 (이름 학번 점수) : ", i + 1);
+    StdType* students = (StdType*)malloc(size * sizeof(StdType)); //동적할당 배열생성
 
+    if(students == NULL){
+        printf("메모리 할당 실패\n");
+        return 1;
     }
 
+
+    for(int i = 0; i < size; i++){
+        printf("[%d] 학생의 정보를 입력하세요 (이름 학번 점수) : ", i + 1);
+        scanf("%s %d %d", students[i].name, &students[i].id, &students[i].score);
+        getchar();
+    }
 
 
     while(1){
-        int input = 0;
-        printf("학번을 입력하세요 (0이면 종료)? ");
+        int input;
+        printf("학번을 입력하세요(0이면 종료) : ");
         scanf("%d", &input);
+        getchar(); //버퍼 비우기
 
-        if(input == (*sptr).id){
-            printf("%c %d %d", (*sptr).name, (*sptr).id, (*sptr).score)
-        }else if(input == 0)
+        if(input == 0)
             break;
+
+        int found = 0;
+        if(input != 0){
+            for(i = 0; i < size; i++){
+                if(input == students[i].id){
+                    printf("%s %d %d\n", students[i].name, students[i].id, students[i].score);
+                    found = 1;
+                }
+            }
+            
+            if(!found){
+                printf("해당 학번의 학생이 없습니다.\n");
+            }
+        }
     }
 
 
 
 
+    free(students);
     return 0;
 }
