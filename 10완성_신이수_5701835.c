11488b1af4c33c20cc06f0287c17af5abaf7cb55
iiisooshin <leesu020806@stu.kmu.ac.kr>
2025-03-29 22:04:16 +0900

    0327_최종

--- 10완성_신이수_5701835.c
+++ 10완성_신이수_5701835.c
@@ -7,17 +7,24 @@
 
 int main(){
 
+    int i, j, k;
 
-    int iArray[2][2];
-    int jArray[2][2];
-    int result[2][2];
+    //동적 메모리 할당(행)
+    int **iArray = (int **)malloc(sizeof(int *) * 2);
+    int **jArray = (int **)malloc(sizeof(int *) * 2);
+    int **result = (int **)malloc(sizeof(int *) * 2);
     
-    int i;
+    //메모리 할당(열)
+    for (i = 0; i < 2; i++) {
+        iArray[i] = (int *)malloc(sizeof(int) * 2);
+        jArray[i] = (int *)malloc(sizeof(int) * 2);
+        result[i] = (int *)malloc(sizeof(int) * 2);
+    }
 
-    int **pptr = (int**)malloc(sizeof(int*) * 2);
-
-
-    int k = 0;
+    if (iArray == NULL || jArray == NULL || result == NULL) { // 메모리 할당 확인
+        printf("메모리 할당 실패\n");
+        return 1;
+    }
 
     printf("첫 번째 2x2 행렬 입력 : \n");
     for(int i = 0; i <= 1; i++){
@@ -41,6 +48,7 @@ int main(){
         }
     }
 
+    k = 0;
     printf("행렬 덧셈 결과 : \n");
     for(int i = 0; i <= 1; i++){
         for(int j = 0; j <= 1; j++){
@@ -50,7 +58,15 @@ int main(){
         }
     }
 
-
+    // 메모리 해제
+    for (i = 0; i < 2; i++) {
+        free(iArray[i]);
+        free(jArray[i]);
+        free(result[i]);
+    }
+    free(iArray);
+    free(jArray);
+    free(result);
 
     return 0;
 }
--- 11완성_신이수_5701835.c
+++ 11완성_신이수_5701835.c
@@ -3,13 +3,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+
 typedef struct StdType{
     char name[20];
     int id;
     int score;
-}StdType;
-
-
+}StdType; //구조체
 
 
 int main(){
@@ -24,14 +23,14 @@ int main(){
 
     if(students == NULL){
         printf("메모리 할당 실패\n");
-        return 1;
+        return 1; //오류 발생시 종료
     }
 
 
     for(int i = 0; i < size; i++){
         printf("[%d] 학생의 정보를 입력하세요 (이름 학번 점수) : ", i + 1);
-        scanf("%s %d %d", students[i].name, &students[i].id, &students[i].score);
-        getchar();
+        scanf("%s %d %d", students[i].name, &students[i].id, &students[i].score); //name은 배열이라 & 불필요
+        getchar(); //버퍼 비우기
     }
 
 
@@ -42,19 +41,19 @@ int main(){
         getchar(); //버퍼 비우기
 
         if(input == 0)
-            break;
+            break; //입력이 0이면 종료
 
-        int found = 0;
+        int found = 0; //입력 학생 유무 파악 변수
         if(input != 0){
             for(i = 0; i < size; i++){
                 if(input == students[i].id){
-                    printf("%s %d %d\n", students[i].name, students[i].id, students[i].score);
+                    printf("%s %d %d\n", students[i].name, students[i].id, students[i].score); //입력한 학생을 찾으면 출력 후 파악 변수 변경
                     found = 1;
                 }
             }
             
             if(!found){
-                printf("해당 학번의 학생이 없습니다.\n");
+                printf("해당 학번의 학생이 없습니다.\n"); //found가 0일때(해당 학생이 없을때) 출력
             }
         }
     }
@@ -62,6 +61,6 @@ int main(){
 
 
 
-    free(students);
+    free(students); //메모리 해제
     return 0;
 }
