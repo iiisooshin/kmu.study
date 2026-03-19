4cbe4f13d83f774f27a824012a28685a1691c3cf
iiisooshin <leesu020806@stu.kmu.ac.kr>
2026-03-19 11:46:03 +0900

    react

--- WebApplication/web_ws/react_ws/src/App.js
+++ WebApplication/web_ws/react_ws/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+
+  let myName = 'iiisoo';
+
   return (
+    // React.createElement('h1', null, '안녕하세요')
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>안녕하세요</h1>
+      <h2>{myName}입니다.</h2>
     </div>
+
   );
 }
 
